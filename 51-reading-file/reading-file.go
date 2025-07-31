cb94fa6257d5f508847ea5301d0c3c62259218b4
hukma23 <hukmaulil23@gmail.com>
2025-07-31 22:34:10 +0700

    belajar reading file

--- 51-reading-file/reading-file.go
+++ 51-reading-file/reading-file.go
@@ -44,4 +44,22 @@ func main() {
 	fmt.Printf("%d bytes @ %d: ", n2, o2)
 	fmt.Printf("%v\n", string(b2[:n2]))
 
+	// Other methods of seeking are relative to the current cursor position,
+	_, err = f.Seek(2, io.SeekCurrent)
+	check(err)
+
+	// and relative to the end of file.
+	_, err = f.Seek(-4, io.SeekEnd)
+	check(err)
+
+	// The io package provides some functions that may be helpful for file reading.
+	// For example, reads like the ones above can be more robustly implemented with ReadAtLeast.
+
+	o3, err := f.Seek(6, io.SeekStart)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Print("%d bytes @ %d: %s\n", n3, o3, string(b3))
+	// ok reading file saya ulangi sekali lagi besok lah, pusing og.
 }
